8af1492843e980b2c18206640eaeae83b18b6bce
Helal Ismail <helal@surveypocket.com>
2013-11-22 15:41:40 +0200

    PAPERV restore point

--- socialize-sdk-android-2.9.7/sdk/src/com/socialize/error/SocializeErrorHandler.java
+++
@@ -1,7 +0,0 @@
-package com.socialize.error;
-
-import android.content.Context;
-
-public interface SocializeErrorHandler {
-	public void handleError(Context context, Exception e);
-}
--- socialize-sdk-android-2.9.7/sdk/src/com/socialize/ui/slider/OnActionBarSliderMoveListener.java
+++
@@ -1,9 +0,0 @@
-package com.socialize.ui.slider;
-
-public interface OnActionBarSliderMoveListener {
-
-	public void onOpen(ActionBarSliderView view);
-
-	public void onClose(ActionBarSliderView view);
-
-}
